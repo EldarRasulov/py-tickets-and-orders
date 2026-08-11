b8ef3c820851b4c536a22ff9a481452d274fe362
Eldar <morddiff@gmail.com>
2026-08-11 21:01:24 +0400

    fix:code2.3

--- services/order.py
+++ services/order.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from django.contrib.auth import get_user_model
 from django.db import transaction
+from django.db.models import QuerySet
 
 from db.models import Order, Ticket
 
